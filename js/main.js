6ae70216ee1824d0063694d83f8ea957a718a6f1
phoenix <phoenix@mihai.Dlink>
2014-08-28 23:09:07 +0300

    bug resolved

--- js/main.js
+++ js/main.js
@@ -37,6 +37,11 @@ $(document).ready(function() {
 		});
 
 });
+$('.keep-open').on({
+    "shown.bs.dropdown": function() { this.closable = false; },
+    "click":             function() { this.closable = true; },
+    "hide.bs.dropdown":  function() { return this.closable; }
+});
 
 
 
