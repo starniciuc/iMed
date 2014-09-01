a5191e44475c1d3a2910989b97737d03af615d34
mstarniciuc <mstarniciuc@mstarniciuc-System-Product-Name>
2014-09-01 15:00:53 +0300

    resolved bug #63

--- js/main.js
+++ js/main.js
@@ -37,10 +37,8 @@ $(document).ready(function() {
 		});
 
 });
-$('.keep-open').on({
-    "shown.bs.dropdown": function() { this.closable = false; },
-    "click":             function() { this.closable = true; },
-    "hide.bs.dropdown":  function() { return this.closable; }
+$("#open-pacient-info").click(function(){
+	$(".btn-group").toggleClass("open");
 });
 
 
