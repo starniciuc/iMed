16be2205a9638729d60802d66aed9d6277c08bd3
Mihai <Mihai@Mihai.Dlink>
2014-09-20 21:18:07 +0300

    add modal

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+$(element).find("[rel='tooltip']").tooltip();
+
 /* Load optimizer*/
 $(".collapse-btn").click(function() {
     var mybtn = $(this);
--- js/normalize.js
+++ js/normalize.js
@@ -1,6 +1,7 @@
 
 require(["jquery.min"], function() {
     require(['bootstrap']);
+    require(['bootstrap-dialog.min']);
     require(['animo.min', 'jquery.sparkline.min', 'fastclick', 'jquery.slimscroll.min', 'bootstrap-slider',
         'chosen.jquery.min', 'bootstrap-filestyle.min'], function() {
         require(['app']);
