9cb4d8754f250589e29d5ed14ea283c9f1d9f9a5
Mihai <Mihai@Mihai.Dlink>
2014-09-21 22:28:52 +0300

    change icons and other bugs

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-$(element).find("[rel='tooltip']").tooltip();
+//$(element).find("[rel='tooltip']").tooltip();
 
 /* Load optimizer*/
 $(".collapse-btn").click(function() {
