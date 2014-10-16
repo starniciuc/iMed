db905de48a4b9f43e9205aeb0383b46bec884489
Mihai <Mihai@Mihai.Dlink>
2014-10-16 16:50:44 +0300

    add page acord

--- js/main.js
+++ js/main.js
@@ -6,21 +6,21 @@
 //$(element).find("[rel='tooltip']").tooltip();
 
 /* Load optimizer*/
-$(".collapse-btn").click(function() {
+$(".collapse-btn").click(function () {
     var mybtn = $(this);
     var par = mybtn.parents("li").children(".collapse");
     par.slideToggle(200);
     $(mybtn).toggleClass("close-menu");
 });
 
-$(function() {
+$(function () {
     $("[data-toggle='tooltip']").tooltip();
 });
-$(document).ready(function() {
-    $('.sidebar-btn').click(function() {
+$(document).ready(function () {
+    $('.sidebar-btn').click(function () {
         $(this).toggleClass("left-20");
     });
-    $(".hide-form").click(function() {
+    $(".hide-form").click(function () {
         if ($(this).text() == 'Ascunde formularul') {
             $(this).text('Arată formularul');
         } else {
@@ -29,35 +29,45 @@ $(document).ready(function() {
     });
 
 });
-$("#open-pacient-info").click(function() {
+$("#open-pacient-info").click(function () {
     $(".btn-group").toggleClass("open");
 });
 
-$(".i-menu").click(function() {
+$(".i-menu").click(function () {
     $("a").removeClass("open-item-menu");
     $(this).toggleClass("open-item-menu");
 });
-$(".hide-btn-tab").click(function() {
+$(".hide-btn-tab").click(function () {
     $(".hide-btn-tab").fadeOut();
 });
-$(".show-btn").click(function() {
+$(".show-btn").click(function () {
     $(".hide-btn-tab").fadeIn();
 });
 if (!('boxShadow' in document.body.style)) {
     document.body.setAttribute('class', 'noBoxShadow');
 }
-$(document).ready(function() {
+$(document).ready(function () {
     var $menu = $(".aside");
     if ($(window).scrollTop() > 70) {
         $menu.css("margin-top", "0");
     }
-    $(window).scroll(function() {
+    $(window).scroll(function () {
 
         margin = 70 - $(this).scrollTop();
         _margin = margin < 0 ? 0 : margin;
         $menu.css("margin-top", _margin);
 
     });//scroll
+    
+    // auto adjust the height of
+    $('#container').on( 'keyup', 'textarea', function (e){
+        $(this).css('height', 'auto' );
+        $(this).height( this.scrollHeight );
+    });
+    $('#container').find( 'textarea' ).keyup();
+    $("#refuz-check").change(function(){
+        $("#accept").slideToggle();
+        $("#refuz").slideToggle();
+    });
 });
-
 
