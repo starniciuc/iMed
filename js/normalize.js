b0c6de0df04dc04f056193d8bce8f809a1ac5d5f
Mihai <Mihai@Mihai.Dlink>
2014-11-06 09:32:14 +0200

    add new page

--- js/normalize.js
+++ js/normalize.js
@@ -3,7 +3,7 @@ require(["jquery.min"], function() {
     require(['bootstrap']);
     require(['bootstrap-dialog.min']);
     require(['animo.min', 'jquery.sparkline.min', 'fastclick', 'jquery.slimscroll.min', 'bootstrap-slider',
-        'chosen.jquery.min', 'bootstrap-filestyle.min'], function() {
+        'chosen.jquery.min', 'bootstrap-filestyle.min', 'moment-with-langs.min', 'bootstrap-datetimepicker.min'], function() {
         require(['app']);
     });
     require(['main']);
